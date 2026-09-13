72d75a8d3c7fb23464b50c872ea7b4393bf1683f
Sachet27 <thesachet123@gmail.com>
2026-09-13 14:20:46 +0545

    added CRUD operations

--- chatapp/base/urls.py
+++ chatapp/base/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns= [
     path('', views.home, name= 'home'),
-    path('room/<int:room_id>', views.room, name= 'room'),
+    path('room/<int:room_id>/', views.room, name= 'room'),
+    path('create-room/', views.create_room, name= 'create_room'),
+    path('update-room/<int:room_id>/', views.update_room, name= 'update_room'),
+    path('delete-room/<int:room_id>/', views.delete_room, name= 'delete_room'),
 ]
