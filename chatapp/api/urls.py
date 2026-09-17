cfd2a52222bdba723e43b2a4f221d92b5c340f0d
Sachet27 <thesachet123@gmail.com>
2026-09-17 18:59:49 +0545

    profile picture system fully implemented (for DEBUG mode)

--- chatapp/api/urls.py
+++ chatapp/api/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns= [
     path('', views.get_routes, name="api_routes"),
     path('rooms/', views.get_rooms, name="rooms"),
-    path('rooms/<int:room_id>', views.get_room, name="room"),
+    path('rooms/<int:room_id>', views.get_room, name="room_details"),
 ]
